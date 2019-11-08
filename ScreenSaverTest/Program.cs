18ff0b79d1bde3b2e168654be99356ef02522ebf
GutoMolinari <gutomolinari@hotmail.com>
2019-11-08 00:29:20 -0300

    - Initial testings;

---
+++ ScreenSaverTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaverTest
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }        
+    }
+}
